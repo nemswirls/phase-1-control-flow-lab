1407c9b45a82f7d0424e856aaadc38e9cada5fc7
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-10-28 22:17:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+    return (`This one is on me!`);
+  }
+  else if (someValue > 400 && someValue< 2000 ){
+  return ('That will be twenty bucks.')
+  }
+  else if (someValue > 2000 && someValue < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (someValue > 2500){
+    return "No can do."
+  }
+  
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }
+  else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  } else if (tip === "not as generous"){
+    return "Thank you."
+  }else{
+    return "Bye."
+  }
 }
